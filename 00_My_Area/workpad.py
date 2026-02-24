d02bb1871aa1d788fc18c366eb10e0019a490eb2
yarnis69 <91457712+yarnis69@users.noreply.github.com>
2026-02-24 17:02:45 +0000

    hello

--- 00_My_Area/workpad.py
+++ 00_My_Area/workpad.py
@@ -1,4 +1,4 @@
-#basic print funtion
+#basic print funtion (what is being printed must be in round brackets)
 print ("hello Dave")
 
 #basic maths with answers output via the print function
@@ -7,3 +7,13 @@ print(5-1) #subtraction
 print(5*2) #multiplication
 print(6/2) #division
 print(5**2) #raise to a power
+
+#create variables and their associated values e.g. time_zone = 'GMT'
+#note: variable name cannot have spaces, cannot start with a number, cannot use python keywords, are case sensitive, and text values must be in quotation marks ('' or "") (strings)
+
+time_zone='GMT'
+name='John Smith'
+age=35
+print(time_zone)
+print(name)
+print(age)
