6b73bd214e45a20892303571ca70b6b6f13263fe
yarnis69 <91457712+yarnis69@users.noreply.github.com>
2026-02-24 16:51:45 +0000

    hi

---
+++ 00_My_Area/workpad.py
@@ -0,0 +1,9 @@
+#basic print funtion
+print ("hello Dave")
+
+#basic maths with answers output via the print function
+print(1+2) #addition
+print(5-1) #subtraction
+print(5*2) #multiplication
+print(6/2) #division
+print(5**2) #raise to a power
