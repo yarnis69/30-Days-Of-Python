0c8b7e18bf4b4fe5fe25c1b44adfffa8cbc5efac
yarnis69 <91457712+yarnis69@users.noreply.github.com>
2026-02-25 09:26:53 +0000

    hi

--- 00_My_Area/workpad.py
+++ 00_My_Area/workpad.py
@@ -1,5 +1,7 @@
 #basic print funtion (what is being printed must be in round brackets)
 print ("hello Dave")
+#can print multiple values by seperating with commas (Python auto adds a space)
+print ('Tom', 'Dick', 'and Harry')
 
 #basic maths with answers output via the print function
 print(1+2) #addition
@@ -10,10 +12,11 @@ print(5**2) #raise to a power
 
 #create variables and their associated values e.g. time_zone = 'GMT'
 #note: variable name cannot have spaces, cannot start with a number, cannot use python keywords, are case sensitive, and text values must be in quotation marks ('' or "") (strings)
-
 time_zone='GMT'
 name='John Smith'
 age=35
 print(time_zone)
 print(name)
 print(age)
+
+#
