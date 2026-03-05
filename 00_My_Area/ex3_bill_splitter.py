316df1b487848c2afd86a60d6437a818f5f3468e
yarnis69 <91457712+yarnis69@users.noreply.github.com>
2026-03-05 13:29:40 +0000

    s

--- 00_My_Area/ex3_bill_splitter.py
+++ 00_My_Area/ex3_bill_splitter.py
@@ -1,1 +1,18 @@
-#
+num_of_friends = 4
+
+appetizers = 37.89
+main_courses = 57.34
+desserts = 39.39
+drinks = 64.21
+
+running_total = appetizers+main_courses+desserts+drinks
+print(f'Total bill so far: {running_total}')
+
+tip = running_total * 0.25
+print(f'Tip amount: {tip}')
+
+running_total += tip
+print(f'Total with tip: {running_total}')
+
+final_bill = running_total / num_of_friends
+print(f'Bill per person: {final_bill}')
