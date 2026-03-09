6ef143310bae0107257e39bd6df9f74eebfbc29b
yarnis69 <91457712+yarnis69@users.noreply.github.com>
2026-03-09 10:30:38 +0000

    a

--- 00_My_Area/Ex5_Travel_Weather_Planner.py
+++ 00_My_Area/Ex5_Travel_Weather_Planner.py
@@ -3,25 +3,22 @@
 distance_mi = 8
 is_raining = False
 has_bike = True
-has_car = True
+has_car = False
 has_ride_share_app = False
 
-#app logic
+#app logic - can I commute
 
 if not distance_mi:
     print('False')
 
-if distance_mi <= 1 and not is_raining:
+elif distance_mi <= 1 and not is_raining:
     print('True')
-else:
-    print('False')
 
-if distance_mi > 1 and distance_mi <= 6 and has_bike and not is_raining:
+elif distance_mi > 1 and distance_mi <= 6 and has_bike and not is_raining:
     print('True')
-else:
-    print('False')
 
-if distance_mi > 6 and (has_car or has_ride_share_app):
+elif distance_mi > 6 and (has_car or has_ride_share_app):
     print('True')
+
 else:
     print('False')
