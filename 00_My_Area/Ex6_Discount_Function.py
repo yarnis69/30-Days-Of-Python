48b10ab5ac91e709c4a0b9a8d2dc5eff14ce4457
yarnis69 <91457712+yarnis69@users.noreply.github.com>
2026-03-09 16:00:57 +0000

    d

--- 00_My_Area/Ex6_Discount_Function.py
+++ 00_My_Area/Ex6_Discount_Function.py
@@ -1,12 +1,23 @@
-#Base discount function
 
 def apply_discount(price,discount):
-
-    discount /= 100
-    return (price * (1 - discount))
     
+    if type(price) != float and type(price) != int:
+        return ("The price should be a number")
+    
+    if type(discount) != float and type(discount) != int:
+        return ("The discount should be a number")
+    
+    if price <=0:
+        return ("The price should be greater than 0")
+    
+    if discount < 0 or discount > 100:
+        return ("The discount should be between 0 and 100")
 
-print (apply_discount(100,50))
+    else:
+        discount = discount/100 * price
+        return (price - discount)
+    
+print (apply_discount(50, 0))
 
 
 
