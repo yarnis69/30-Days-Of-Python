f8884af888091c24c7e3804c5c2ec3aab3c8c6c0
yarnis69 <91457712+yarnis69@users.noreply.github.com>
2026-03-11 10:01:39 +0000

    lk

--- 00_My_Area/Python_Advanced.py
+++ 00_My_Area/Python_Advanced.py
@@ -2,14 +2,20 @@
 
 #refresher on lists, tuples and ranges
 
-my_list = ['Tom', 'Dick', 'Harry']
 #lists are not immutable (can update individual values) and can have repeated values
+my_list = ['Tom', 'Dick', 'Harry']
 
-my_tuple = ('Tom', 'Dick', 'Harry')
 #tuples are immutable (cannot update individual values) and can have repeated values
+my_tuple = ('Tom', 'Dick', 'Harry')
+
+#you can use indexing to extract individual values from lists and tupels
+#indexing starts at 0, and you can use negative indexing to index from the end
+#you can also use a colon (:) to define a first and last value to extract, to pull a range of values
+print(my_list[0])
+print(my_tuple[-1])
+print(my_list[0:1])
 
 #you can update individual entries in a list, but the same operation would error in a tuple
-
 my_list [0] = 'Frank'
 print(my_list) # now returns Frank, Dick, Harry
 
@@ -19,5 +25,4 @@ my_range = range(1, 11, 1)
 
 #if you were to convert a range object to a list, it returns all values in that range
 print(list(my_range)) # returns 1,2,3,4,5,6,7,8,9,10
-
 
