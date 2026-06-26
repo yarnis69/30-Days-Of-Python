df4e74ce2b8cb997be4f6ca68ad683ee5b5cc35d
yarnis69 <91457712+yarnis69@users.noreply.github.com>
2026-06-26 13:25:08 +0000

    dfsa

--- 00_My_Area/Python_Advanced.py
+++ 00_My_Area/Python_Advanced.py
@@ -1,6 +1,7 @@
 # Loops
 
-#refresher on lists, tuples and ranges
+#refresher on lists, tuples and ranges (iterables)
+#iterables are a type of objects you can loop over (one value at a time)
 
 #lists are not immutable (can update individual values) and can have repeated values
 my_list = ['Tom', 'Dick', 'Harry']
@@ -43,12 +44,14 @@ my_range = range(1, 11, 1)
 print(list(my_range)) # returns 1,2,3,4,5,6,7,8,9,10
 
 
+#advanced list functions
+
+
 #you can nest lists inside lists
 my_nested_list = ["red", "green", ["light blue", "dark blue"]]
 
 #you can then provide an index for the outer and inner list to retrive an entry
 print(my_nested_list[2][1]) # returns 'dark blue'
-
 
 #you can assign list entries to new variables in turn, known as unpacking lists
 employee_481 = ['Matthew', '36', 'Security Consultant']
@@ -59,3 +62,71 @@ print(f'{name} is {age} and works as a {job_title}')
 employee_481 = ['Matthew', '36', 'Security Consultant', 'Legend']
 name, age, *job_title = employee_481 # job_title is now assigned a list containing the last two values
 print(f'{name} is {age} and works as a {job_title}')
+
+# .append() is a method used to add a new entry to a list
+my_list = [1,2,3,4]
+my_list.append(5)
+print(my_list) # now returns [1,2,3,4,5]
+
+#if you were to use .append() to add a list to another list, it would nest it a single entry
+my_list = [1,2,3,4]
+my_second_list = [5,6,7,8]
+my_list.append(my_second_list)
+print(my_list) # returns [1,2,3,4,[5,6,7,8]]
+
+# .extend() prevents this nesting, adding each value as indivual list entries
+my_list = [1,2,3,4]
+my_second_list = [5,6,7,8]
+my_list.extend(my_second_list)
+print(my_list) # returns [1,2,3,4,5,6,7,8]
+
+# .insert() is used to add a new entry to a list at a certain point (without overiding an exisiting entry)
+# syntax is .insert(index of insert, new entry value)
+my_list = [1,2,3,4,5,6]
+my_list.insert(3,"midpoint") #note the insert index pushes the value that was there up by one
+print(my_list) #returns [1,2,3,midpoint,4,5,6]
+
+# .remove() deletes a single value from a list - passing the value as the argument
+#  syntax is .remove(value to be deleted) - note this will only delete the FIRST OCCURRENCE OF THE VALUE
+my_list = [10,20,30,40,50]
+my_list.remove(40)
+print(my_list) # now returns [10,20,30,50]
+
+# .pop() deletes a single value from a list - passing the index as the argument
+# if no index is specified, it removes the last entry from the list
+my_list = [1,2,3,4,5]
+my_list.pop(0)
+print(my_list) # now returns [2,3,4,5]
+
+# .clear() deletes all entries in a list
+my_list = [1,2,3,4,5]
+my_list.clear()
+print(my_list) # returns an empty list
+
+# .sort() sorts a list numerically
+my_list = [2,4,1,3,5]
+my_list.sort()
+print(my_list) # returns [1,2,3,4,5]
+
+#this also works on letters
+my_list = ['a','c','b']
+my_list.sort()
+print(my_list) # returns [a,b,c]
+
+# sorted() also sorts a list, but creates a new list rather than updating the existing
+# syntax is .sorted(existing list to be sorted)
+my_list = [1,3,2,5,4]
+my_sorted_list = sorted(my_list)
+print(my_sorted_list) # note my_list is unchanged 
+
+# .reverse() reverses the indexing of a list (1st becomes last etc)
+my_list = [1,2,3,4,5]
+my_list.reverse()
+print(my_list) # now returns [5,4,3,2,1]
+
+# .index() is used to find the index of the first occurance of a value
+my_list = [1,2,3,4,5]
+my_list.index(4) # returns an index of 3
+
+# advanced tuple functions
+
