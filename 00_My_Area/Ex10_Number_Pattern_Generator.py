a923629b29f8969fcc77a4c182a991ed8229e896
yarnis69 <91457712+yarnis69@users.noreply.github.com>
2026-07-17 17:05:31 +0000

    adsfsdsw

--- 00_My_Area/Ex10_Number_Pattern_Generator.py
+++ 00_My_Area/Ex10_Number_Pattern_Generator.py
@@ -2,15 +2,17 @@ def number_pattern(n):
     
     output = ''
 
-    if type(n) == int:
+    if type(n) != int:
+        print("Argument must be an integer value")
 
+    elif n < 1:
+        print("Argument must be an integer greater than 0")
+
+    else:
         for number in range(1,n+1):
             output += str(number) + ' '
         
         print(output)
+        
 
-    else:
-        print("Argument must be an integer value")
-
-
-number_pattern('a')
+number_pattern(10)
