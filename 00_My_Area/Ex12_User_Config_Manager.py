99405a4267b86e3401d390498c6e600c1f4eaeb7
yarnis69 <91457712+yarnis69@users.noreply.github.com>
2026-07-29 13:24:40 +0000

    dsaadf

--- 00_My_Area/Ex12_User_Config_Manager.py
+++ 00_My_Area/Ex12_User_Config_Manager.py
@@ -10,8 +10,26 @@ def add_setting(settings_dict, updated_setting):
         settings_dict.update({updated_setting[0]:updated_setting[1]})
         print(f"Setting '{updated_setting[0]}' added with value '{updated_setting[1]}' successfully!")
 
+
+
+def update_setting(settings_dict, updated_setting):
+
+    updated_setting = tuple(key_value.lower() for key_value in updated_setting)
+
+    if updated_setting[0] in settings_dict.keys():
+        settings_dict.update({updated_setting[0]:updated_setting[1]})
+        print(f"Setting '{updated_setting[0]}' updated to '{updated_setting[1]}' successfully!")
+
+    else:
+        print(f"Setting '{updated_setting[0]}' does not exist! Cannot update a non-existing setting.")
+
+
+
+def delete_setting(settings_dict, setting_key):
+    pass
+
+    
+
 #test_settings_dict = {'abc':'efg', 'ert':'hrq'}
-#test_setting_tuple = ('ZZZ', 'YYY')
-#add_setting(test_settings_dict,test_setting_tuple)
-
-
+#test_setting_tuple = ('ABC', 'YYY')
+#update_setting(test_settings_dict,test_setting_tuple)
